0447815a49a2dd1b4bdacbe37f381b661c0c961c
Дружинин Михаил Юрьевич <druzhininmy@gmail.com>
2024-10-07 04:09:49 +0300

    fix Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,30 +4,33 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public static void main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+@SuppressWarnings("ExplicitToImplicitClassMigration")
+public class Main {
+    public static void main(String[] ignoredArgs) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    try {
-        ServerSocket serverSocket = new ServerSocket(4221);
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
 
-        // Since the tester restarts your program quite often, setting SO_REUSEADDR
-        // ensures that we don't run into 'Address already in use' errors
-        serverSocket.setReuseAddress(true);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
 
-        Socket socket = serverSocket.accept(); // Wait for connection from client.
-        System.out.println("accepted new connection");
+            Socket socket = serverSocket.accept(); // Wait for connection from client.
+            System.out.println("accepted new connection");
 
-        OutputStream outputStream = socket.getOutputStream();
+            OutputStream outputStream = socket.getOutputStream();
 
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-        dataOutputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-        dataOutputStream.flush();
-        dataOutputStream.close();
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            dataOutputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            dataOutputStream.flush();
+            dataOutputStream.close();
 
-        socket.close();
+            socket.close();
 
-    } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
     }
 }
